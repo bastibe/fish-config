352950e4151b138bcc16e48d067303ad937b5e9d
Bastian Bechtold <basti@bastibe.de>
2015-12-02 11:28:51 +0100

    now runs emacs or emacsclient

--- config.fish
+++ config.fish
@@ -2,10 +2,11 @@ set MINICONDAPATH ~/.miniconda3
 set PATH $MINICONDAPATH/bin $PATH
 source ~/.config/fish/conda.fish
 
-alias emacs /usr/local/bin/emacs
+alias emacs /usr/bin/emacs
 alias magit "emacs -nw --eval \"(magit-status \\\".\\\")\""
 
-set -gx EDITOR /usr/local/bin/emacs
+set -gx EDITOR "/usr/bin/emacsclient --alternate-editor=/usr/bin/emacs"
+set -gx ALTERNATE_EDITOR /usr/bin/emacs
 
 # FIXME: These don't work here, so I put them in
 #        functions/fish_prompt.fish as well
